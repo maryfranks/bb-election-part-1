0b25153fcde0c94201ad6cc26ed13759757aed3d
maryfranks <maryfranks@gmail.com>
2017-08-22 15:42:47 -0400

    add list to index.html with candidate names and number of votes

--- election.js
+++ election.js
@@ -1,5 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  $.ajax({
+      url: "https://bb-election-api.herokuapp.com/",
+      method: "GET",
+      dataType: "json"
+    }).done(function (responseData) {
+
+      var candidatesArray = responseData.candidates;
+      console.log(candidatesArray);
+      for (var i = 0; i < candidatesArray.length; i++) {
+        var name = candidatesArray[i]['name']
+        var votes = candidatesArray[i]['votes']
+
+        var newListItem = document.createElement('li');
+        newListItem.innerText = name + ", votes: " + votes;
+        var mainList = document.getElementById('candidates');
+        mainList.appendChild(newListItem);
+      }
+
+    });
 
 });
